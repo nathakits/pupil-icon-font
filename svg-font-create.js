60497451aa515853b31399953d7c1fd9c7689385
Nathakit Sae-Tan <nathakit.sae-tan@cuinda.com>
2018-04-28 13:37:36 +0700

    refactoring script

--- svg-font-create.js
+++ svg-font-create.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-
 'use strict';
 
 var fs        = require('fs');
@@ -11,7 +10,28 @@ var Domparser      = require('xmldom').DOMParser;
 var ArgumentParser = require('argparse').ArgumentParser;
 var SvgPath   = require('svgpath');
 
+////////////////////////////////////////////////////////////////////////////////
 
+var parser = new ArgumentParser({
+  addHelp: true,
+  description: 'Pupil Labs internal tool. Join multiple fonts to single one and create JS configs for processing'
+});
+parser.addArgument([ '-i', '--input_fonts' ], { help: 'Input fonts paths', required: true, nargs : '+' });
+parser.addArgument([ '-o', '--output' ], { help: 'Output font file path', required: true });
+// parser.addArgument([ '-s', '--output_server' ], { help: 'Output server config path' });
+
+var args = parser.parseArgs();
+
+////////////////////////////////////////////////////////////////////////////////
+
+// template for scaled svgs
+var svgTemplate = _.template(
+  '<svg height="<%= svg.height %>" width="<%= svg.width %>" viewBox="<%= svg.viewbox %>" xmlns="http://www.w3.org/2000/svg">\n' +
+  '<path d="<%= svg.d %>"/>\n' +
+  '</svg>'
+);
+
+// font template
 var svgFontTemplate = _.template(
     '<?xml version="1.0" standalone="no"?>\n' +
     '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' +
@@ -45,23 +65,21 @@ var svgFontTemplate = _.template(
     '</svg>'
   );
 
-var svgTemplate = _.template(
-  '<svg height="<%= svg.height %>" width="<%= svg.width %>" viewBox="<%= svg.viewbox %>" xmlns="http://www.w3.org/2000/svg">\n' +
-  '<path d="<%= svg.d %>"/>\n' +
-  '</svg>'
-);
+////////////////////////////////////////////////////////////////////////////////
 
+
+// parse svg to object attributes
 function parseSvgImage(data, filename) {
 
   var parser = new Domparser();
-  var doc = parser.parseFromString(data, "image/svg+xml");
+  var doc = parser.parseFromString(data, 'image/svg+xml');
   var svg = doc.getElementsByTagName('svg')[0];
 
   if (!svg.hasAttribute('height')) {
-    throw filename ? 'Missed height attribute in ' + filename : 'Missed height attribute';
+    throw `Missed height attribute in ${filename}`;
   }
   if (!svg.hasAttribute('width')) {
-    throw filename ? 'Missed width attribute in ' + filename : 'Missed width attribute';
+    throw `Missed width attribute in ${filename}`;
   }
 
   var height = svg.getAttribute('height');
@@ -76,54 +94,34 @@ function parseSvgImage(data, filename) {
 
   if (path.length > 1) {
     for (let i = 0; i < path.length; i++) {
-      const element = path[i];
+      const paths = path[i];
       // remove svgs that have bounding box
-      if ( element.getAttribute('d').match(/M0.+/g) ) {
-        svg.removeChild(element);
+      if ( paths.getAttribute('d').match(/M0.+/g) ) {
+        svg.removeChild(paths);
       }
-      console.log('Multiple paths removed: ' + filename);
+      console.log(`Multiple paths removed: ${filename}`);
    }
   }
   if (path.length === 0) {
-    throw 'No path data found' + (filename ? ' (' + filename + ' ' : '');
+    throw `No path data found ${filename}`;
   }
 
   path = path[0];
 
   var d = path.getAttribute('d');
 
-  var transform = '';
+  // var transform = '';
 
-  if (path.hasAttribute('transform')) {
-    transform = path.getAttribute('transform');
-  }
+  // if (path.hasAttribute('transform')) {
+  //   transform = path.getAttribute('transform');
+  // }
 
-  return { height, width, d, transform, viewbox };
+  return { height, width, d, viewbox };
 }
 
-var parser = new ArgumentParser({
-  addHelp: true,
-  description: 'Fontello internal tool. Join multiple fonts to single one and create JS configs for processing'
-});
-parser.addArgument([ '-i', '--input_fonts' ], { help: 'Input fonts paths', required: true, nargs : '+' });
-parser.addArgument([ '-o', '--output' ], { help: 'Output font file path', required: true });
-parser.addArgument([ '-s', '--output_server' ], { help: 'Output server config path' });
-
-var args = parser.parseArgs();
-
-// server config, to build svg fonts
-// contains uid hash + svg paths, to generate font quickly
-var configServer = {
-  icons : {},
-  fonts : {},
-  metas : {}
-};
-
-////////////////////////////////////////////////////////////////////////////////
-// TODO
-// update function
-function resize_icons() {
-  var file_name = path.join('./svgs', 'test' + '.svg');
+// scale svg if it's from other icon sets
+function scale_icons() {
+  var file_name = path.join('./svgs', '' + '.svg');
   var file_read = fs.readFileSync(file_name, 'utf8');
   var svg = parseSvgImage(fs.readFileSync(file_name, 'utf8'), file_name);
 
@@ -142,20 +140,46 @@ function resize_icons() {
   svg.height = viewbox;
   svg.width = viewbox;
   svg.viewbox = `0 0 ${viewbox} ${viewbox}`;
-  console.log(svg)
 
-  var svg_resize = svgTemplate({svg});
+  var svg_resized = svgTemplate({svg});
 
-  fs.writeFileSync('./svgs/test.svg', svg_resize, 'utf8');
+  fs.writeFileSync('./svgs/test.svg', svg_resized, 'utf8');
 }
 
-resize_icons();
+
+
+
+// server config, to build svg fonts
+// contains uid hash + svg paths, to generate font quickly
+// var configServer = {
+//   icons : {},
+//   fonts : {},
+//   metas : {}
+// };
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 // Clean up script and remove config file
 // try to use vanilla js
 
 // Scan sources
 // we don't need to loop - but could keep it in if we ever want to add more than one font
+
+var data = {}; // or wherever you get it from)
+
+function svg2ttf(argument) {
+  for (var i = Things.length - 1; i >= 0; i--) {
+      Things[i]
+    }  
+
+
+    for (var key in defaultData){
+       data[key] = data[key] || defaultData[key];
+    }
+} 
+
+
 _.forEach(args.input_fonts, function (fontDir) {
   // Iterate each font
   var cfg = yaml.load(fs.readFileSync(path.resolve('./config.yml'), 'utf8'));
